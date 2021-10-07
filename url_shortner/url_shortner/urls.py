2d3463e0e59f72ff603c7b7a5a78ed2a36f46dc3
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-10-07 11:48:57 +0545

    Added favicon

--- url_shortner/url_shortner/urls.py
+++ url_shortner/url_shortner/urls.py
@@ -3,6 +3,9 @@ from django.urls import path,include
 from django.urls.conf import include
 from Shortner import views
 from . import settings
+import os
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls), # keep
@@ -17,5 +20,23 @@ urlpatterns = [
     path('forum/',include('forums.urls')),
     path('chat/',include('chat.urls')),
     path('',include('Shortner.urls')),
+]
 
+# Serve the static HTML
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    url(r'^site/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(BASE_DIR, 'site'),
+         'show_indexes': True},
+        name='site_path'
+        ),
+]
+
+# Serve the favicon - Keep for later
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
+    ),
 ]
