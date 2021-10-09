423293c947b02c4ce69b01e2d9ea35013223b68a
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-10-09 20:20:53 +0545

    Added postgresql database

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -91,6 +91,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django_studybud',
+#         'USER': 'postgres',
+#         'PASSWORD': 'rabina00',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
