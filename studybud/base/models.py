dc6725524bbb17137d154a4b41c414d7e6c90b7a
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-10-09 15:54:34 +0545

    Updated studybud

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
-    username = models.CharField(max_length=100,null=False)
+    username = models.CharField(max_length=100,null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
