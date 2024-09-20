82c8dc5b726c5c4f54f0525ad8f780c4a1405bcd
Rabin Phaiju <rabinphaiju15@gmail.com>
2024-09-20 20:12:48 +0545

    added admin view in url_shortner laptop app

--- url_shortner/laptop/admin.py
+++ url_shortner/laptop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Laptop,Brand
+
+admin.site.register(Brand)
+admin.site.register(Laptop)
--- url_shortner/laptop/models.py
+++ url_shortner/laptop/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 class Brand(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.name
 
 class Laptop(models.Model):
     name = models.CharField(max_length=200)
     brand = models.ForeignKey('Brand',on_delete=models.CASCADE,null=False)
     weight = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
