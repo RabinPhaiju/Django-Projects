812fac4021d802aeb33df88e00d6cf63eb75fb1b
Rabin Phaiju <rabinphaiju15@gmail.com>
2024-09-20 19:47:51 +0545

    added django_debug_toolbar

--- django5learn/django5learn/settings.py
+++ django5learn/django5learn/settings.py
@@ -1,6 +1,7 @@
 # Django 5.1.1.
 
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +92,23 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# For debug_toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+TESTING = "test" in sys.argv
+if not TESTING:
+    INSTALLED_APPS = [
+        *INSTALLED_APPS,
+        "debug_toolbar",
+    ]
+    MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+        *MIDDLEWARE,
+    ]
+
 
 
 # Internationalization
--- django5learn/django5learn/urls.py
+++ django5learn/django5learn/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include,path
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 """
 Whenever Django encounters include(), it chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
@@ -9,3 +11,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
 ]
+
+
+# for debug toolbar
+if not settings.TESTING:
+    urlpatterns = [
+        *urlpatterns,
+    ] + debug_toolbar_urls()
